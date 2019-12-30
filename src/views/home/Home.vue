dc2fb34119418f640b590460afe69184f1437251
shm <avivahuiminshi@gmail.com>
2019-12-30 16:57:59 +0800

    add tab bar and navigation bar on home page

---
+++ src/views/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="home">
+    <!--导航栏部分-->
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <!--轮播图部分-->
+
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        keywords: [],
+        dKeyword: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+</style>
